e667235e2ecc7b8e10c15d21cd44ae4509bc2d27
Aga <aga@cottagelabs.com>
2025-03-09 19:40:34 +0000

    working version

--- src/components/parallaxSimulation/ParallaxSimulation.js
+++ src/components/parallaxSimulation/ParallaxSimulation.js
@@ -1,293 +1,327 @@
 import React, { useEffect, useRef, useState } from 'react';
 import { roundToTwoDigits } from '../../lib';
 
+const canvasWidth = 300;
+const canvasHeight = 400;
+
 export const ParallaxSimulation = ({distance_pc, px}) => {
+
+// Constants and variables
+  const dTheta_deg = 1;
+  const yOMin = 70;
+  const yOMax = 230;
+  const xSun = 150;
+  const ySun = 340;
+  const annOff = 60;
+  const inc_deg = 7;
+
+
+  const objectPos = (d_pc) => {
+    // Distance in parsecs
+    const A = 500; // Adjust based on visualization size
+    const B = 850; // Adjust based on visualization offset
+
+    // Define minimum and maximum distances in parsecs
+    const dMin_pc = 3.4;
+    const dMax_pc = 8401;
+
+    // Apply logarithmic transformation to distances
+    const log_d_pc = A * Math.log10(d_pc) + B;
+    const log_dMin_pc = A * Math.log10(dMin_pc) + B;
+    const log_dMax_pc = A * Math.log10(dMax_pc) + B;
+
+    // Map logarithmic distance to pixel range
+    const tmp = (log_d_pc - log_dMin_pc) * (yOMax - yOMin) / (log_dMax_pc - log_dMin_pc);
+    let yO = yOMax - tmp;
+    return yO;
+  }
+
+  const distToPix = (d_pc) => {
+    // Calculate pixel distance
+    return ySun - objectPos(d_pc);
+  };
+
+  const RtoPix = (d_pc) => {
+    return distToPix(1) - distToPix(0.5);
+  }
+
+  const calculatePositions = () => {
+    // Separate function for calculations
+    theta_deg.current = (theta_deg.current + dTheta_deg) % 360;
+    const theta_rad = theta_deg.current * Math.PI / 180;
+    xE.current = animationParamsRef.current.R * Math.sin(theta_rad);
+    yE.current = animationParamsRef.current.R * Math.cos(theta_rad) * Math.sin(inc_deg * Math.PI / 180);
+  };
+
+  console.log("Distance: ", distance_pc);
+  console.log("Parallax: ", px);
   const mainCanvasRef = useRef(null);
   const viewCanvasRef = useRef(null);
   const animationFrameId = useRef(null);
   const isMounted = useRef(false);
-  const inclination = 7;
-  console.log("Distance: ", distance_pc);
-  console.log("Parallax: ", px);
+  const animationParamsRef = useRef({
+    R: RtoPix(distance_pc),
+    d_pix: distToPix(distance_pc),
+    yO: objectPos(distance_pc),
+  });
+  const theta_deg = useRef(0);
+  const xE = useRef(0);
+  const yE = useRef(0);
+  const [sunImgLoaded, setSunImgLoaded] = useState(false);
+  const [bgImgLoaded, setBgImgLoaded] = useState(false);
+
+  // Load images once
+  const sunImg = useRef(new Image());
+  const bgImg = useRef(new Image());
+
+  useEffect(() => {
+    sunImg.current.src = '/assets/images/sun.png';
+    sunImg.current.onload = () => setSunImgLoaded(true);
+
+    bgImg.current.src = '/assets/images/star_field.png';
+    bgImg.current.onload = () => setBgImgLoaded(true);
+  }, []);
+
+    // If distance_pc changes, update initial animation parameters
+    useEffect(() => {
+      animationParamsRef.current = {
+        R: RtoPix(distance_pc),
+        d_pix: distToPix(distance_pc),
+        yO: objectPos(distance_pc)
+      };
+    }, [distance_pc]);
+
+  const drawBackground = (ctx) => {
+    ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+    ctx.drawImage(bgImg.current, 0, 0, ctx.canvas.width, ctx.canvas.height);
+  };
+
+  const drawEarth = (ctx, xE_tl, yE_tl) => {
+    ctx.beginPath();
+    ctx.arc(xE_tl, yE_tl, 5, 0, 2 * Math.PI);
+    const grad = ctx.createRadialGradient(xE_tl - 1, yE_tl - 2, 1, xE_tl, yE_tl, 5);
+    grad.addColorStop(0, "#b3bdf8");
+    grad.addColorStop(1, "blue");
+    ctx.fillStyle = grad;
+    ctx.fill();
+  };
+
+  const drawObject = (ctx, xO_tl, yO_tl) => {
+    ctx.beginPath();
+    ctx.arc(xO_tl, yO_tl, 5, 0, 2 * Math.PI);
+    const grad = ctx.createRadialGradient(xO_tl - 1, yO_tl - 2, 1, xO_tl, yO_tl, 5);
+    grad.addColorStop(0, "white");
+    grad.addColorStop(1, "green");
+    ctx.fillStyle = grad;
+    ctx.fill();
+  };
+
+  const drawForegroundObject = (ctx, xO_tl, yO_tl) => {
+    xO_tl = xSun
+    ctx.beginPath();
+    ctx.arc(xO_tl, yO_tl, 5, 0, 2*Math.PI);
+    const grad = ctx.createRadialGradient(xO_tl-1, yO_tl-2, 1,
+                                         xO_tl, yO_tl, 5);
+    grad.addColorStop(0, "white");
+    grad.addColorStop(1, "green");
+    ctx.fillStyle = grad;
+    ctx.fill();
+  };
+
+  const drawAngleAnnotation = (ctx, xO_tl, yO_tl) => {
+    const ang = Math.atan(animationParamsRef.current.R/animationParamsRef.current.d_pix)
+    const parAngle_asec = 1/distance_pc;
+    const xArcEnd = xO_tl + annOff * Math.sin(ang)
+    const yArcEnd = yO_tl- annOff * Math.cos(ang)
+    ctx.beginPath();
+    ctx.setLineDash([5, 5])
+    ctx.strokeStyle = "yellow";
+    ctx.lineWidth=1.0;
+    ctx.moveTo(xO_tl, yO_tl);
+    ctx.lineTo(xArcEnd, yArcEnd);
+    ctx.moveTo(xO_tl, yO_tl);
+    ctx.lineTo(xO_tl, yO_tl-annOff);
+    ctx.stroke();
+    ctx.beginPath();
+    ctx.setLineDash([1, 0])
+    ctx.lineWidth=3.0;
+    ctx.arc(xO_tl, yO_tl, annOff, -Math.PI/2, +ang-Math.PI/2);
+    ctx.stroke();
+    ctx.fillStyle = "yellow";
+    ctx.font = "14px Verdana";
+    ctx.textAlign="start"; 
+    var annTxt = "Angle = " + parAngle_asec.toFixed(2) + "''"
+    ctx.fillText(annTxt, xO_tl+annOff*Math.sin(ang)+ annOff*0.1,
+                  yO_tl-annOff*Math.cos(ang)/2);
+  };
+
+  const drawProjectedOrbit = (ctx, R1) => {
+    ctx.beginPath();
+    ctx.setLineDash([7, 3])
+    ctx.strokeStyle = 'grey';
+    ctx.lineWidth=1.5;
+    ctx.ellipse(canvasWidth/2, canvasHeight/2, R1,
+                  R1 * Math.cos(inc_deg * Math.PI / 180), 0, 0, 2*Math.PI);
+    ctx.stroke();
+  }
+
+  const drawAngleSweepLine = (ctx, xO_tl, yO_tl, xE_tl, yE_tl) => {
+    let xH_tl = xSun - (animationParamsRef.current.yO * xE.current) / (yE_tl - animationParamsRef.current.yO);
+    ctx.beginPath();
+    ctx.moveTo(xO_tl, yO_tl);
+    ctx.lineTo(xH_tl, 0);
+    ctx.moveTo(xO_tl, yO_tl);
+    ctx.lineTo(xE_tl, yE_tl);
+    ctx.setLineDash([7, 3])
+    ctx.lineWidth=2.0;
+    ctx.strokeStyle = 'grey';
+    ctx.stroke();
+  }
+
+  const drawProjectedAngleLine = (ctx, R1) => {
+    ctx.beginPath();
+    ctx.setLineDash([3, 3])
+    ctx.strokeStyle = 'yellow';
+    ctx.lineWidth=1.0;
+    ctx.moveTo(canvasWidth/2, canvasHeight/2);
+    ctx.lineTo(canvasWidth/2+R1, canvasHeight/2);
+    ctx.stroke();
+  }
+
+  const drawViewOfObject = (ctx, xP_tl, yP_tl) => {
+    
+    ctx.beginPath();
+    ctx.arc(xP_tl, yP_tl, 5, 0, 2*Math.PI);
+    const grad = ctx.createRadialGradient(xP_tl-1, yP_tl-2, 1,
+                                          xP_tl, yP_tl, 5);
+    grad.addColorStop(0, "white");
+    grad.addColorStop(1, "green");
+    ctx.fillStyle = grad;
+    ctx.fill();
+  }
+
+  const printMonths = (ctx) => {
+    var months = ["January", "February", "March", "April", "May", "June",
+      "July", "August", "September", "October", "November",
+      "December" ]
+    ctx.beginPath();
+    ctx.fillStyle = "grey";
+    ctx.font = "14px Verdana";
+    ctx.textAlign="start";
+    ctx.fillText(months[Math.floor(theta_deg.current/30)], 8,
+            canvasHeight-8)
+  }
+
+  const annotate = (ctx, text, top, left) => {
+    // Final annotations
+    ctx.beginPath();
+    ctx.fillStyle = "white";
+    ctx.font = "14px Verdana";
+    ctx.textAlign="start"; 
+    ctx.fillText(text, left, top);
+  }
+
+  const drawSun = (ctx) => {
+    ctx.drawImage(sunImg.current, xSun-sunImg.current.width/4, ySun-sunImg.current.height/4,
+                  sunImg.current.width/2, sunImg.current.height/2);
+  }
+
+  const drawNearEarthOrbit = (ctx) => {
+    ctx.ellipse(xSun, ySun, animationParamsRef.current.R, animationParamsRef.current.R * Math.sin(inc_deg * Math.PI / 180.0),
+                 0, 0, Math.PI)
+    ctx.setLineDash([7, 3])
+    ctx.lineWidth=2;
+    ctx.strokeStyle = 'grey';
+    ctx.stroke();
+  }
+         
+  const drawFarEarthOrbit = (ctx) => {
+    ctx.ellipse(xSun, ySun, animationParamsRef.current.R, animationParamsRef.current.R * Math.sin(inc_deg * Math.PI / 180),
+                 0, Math.PI, 0)
+    ctx.setLineDash([7, 3])
+    ctx.lineWidth=1.5;
+    ctx.strokeStyle = 'grey';
+    ctx.stroke();
+
+    // Annotate the Earth
+    ctx.font = "14px Verdana";
+    ctx.fillStyle = '#6b72f8';
+    ctx.textAlign="end"; 
+    ctx.fillText("Earth", xSun-animationParamsRef.current.R-10, ySun+4)
+    ctx.textAlign="start"; 
+  
+  }
 
   useEffect(() => {
     isMounted.current = true;
-    const mainCanvas = mainCanvasRef.current;
-    const viewCanvas = viewCanvasRef.current;
-    const ctx1 = mainCanvas.getContext('2d');
-    const ctx2 = viewCanvas.getContext('2d');
-
-    // Load the image of the Sun
-    const sunImg = new Image();
-    sunImg.src = '/assets/images/sun.png';
-
-    let backgroundImg = new Image();
-    backgroundImg.src = '/assets/images/star_field.png';
-
-    // Constants and variables
-    let xE = 0;
-    let yE = 0;
-    let d_pix;
-    let xO = 0, yO;
-    let theta_deg = 0;
-    const dTheta_deg = 1;
-    let R;
-    const yOMin = 70;
-    const yOMax = 230;
-    const xSun = 150;
-    const ySun = 340;
-    const annOff = 60;
-    const inc_deg = 7;
-
-    const distToPix = (d_pc) => {
-      // Distance in parsecs
-      const A = 500; // Adjust based on visualization size
-      const B = 850; // Adjust based on visualization offset
-  
-      // Define minimum and maximum distances in parsecs
-      const dMin_pc = 3.4;
-      const dMax_pc = 8401;
-  
-      // Apply logarithmic transformation to distances
-      const log_d_pc = A * Math.log10(d_pc) + B;
-      const log_dMin_pc = A * Math.log10(dMin_pc) + B;
-      const log_dMax_pc = A * Math.log10(dMax_pc) + B;
-  
-      // Map logarithmic distance to pixel range
-      const tmp = (log_d_pc - log_dMin_pc) * (yOMax - yOMin) / (log_dMax_pc - log_dMin_pc);
-      yO = yOMax - tmp;
-  
-      // Calculate pixel distance
-      return ySun - yO;
-  };
-
-    const RtoPix = (d_pc) => {
-      const a = distToPix(1);
-      const b = distToPix(0.5);
-      const R = a - b;
-      console.log(R);
-      return R;
-    }
-  
+    const ctx1 = mainCanvasRef.current.getContext('2d');
+    const ctx2 = viewCanvasRef.current.getContext('2d');
 
     const drawAll = () => {
-      // Update the object distance from the slider
-      d_pix = distToPix(distance_pc);
-      R = RtoPix(distance_pc);
+
+      const { R, yO, d_pix } = animationParamsRef.current;
     
       // Earth position relative to top-left
-      let xE_tl =  xSun + xE
-      let yE_tl =  ySun + yE
+      let xE_tl =  xSun + xE.current;
+      let yE_tl =  ySun + yE.current;
     
       // Object position relative to top-left
-      let xO_tl = xSun + xO
-      let yO_tl = yO
-
-      // Projected horizon position, relative to top-left
-      let xH_tl = xSun - xE * yO / (yE_tl - yO)
+      let xO_tl = xSun;
+      let yO_tl = yO;
 
       // Projected object motion
-      let R1 = R * yO/ d_pix
-      let theta_rad = theta_deg * Math.PI / 180
+      let R1 = R * yO / d_pix
+      let theta_rad = theta_deg.current * Math.PI / 180
       let xP = R1 * Math.sin(-theta_rad)
       let yP = R1 * Math.cos(theta_rad) * Math.cos(inc_deg * Math.PI / 180)
-      let xP_tl = viewCanvas.width/2 + xP
-      let yP_tl = viewCanvas.height/2 + yP
-    
-      // Clear both canvas for a new frame
-      ctx1.clearRect(0, 0, mainCanvas.width, mainCanvas.height);   
-      ctx2.clearRect(0, 0, viewCanvas.width, viewCanvas.height);   
-      
-      ctx1.drawImage(backgroundImg, 0, 0, mainCanvas.width, mainCanvas.height);
-      ctx2.drawImage(backgroundImg, 0, 0, mainCanvas.width, mainCanvas.height);
+      let xP_tl = canvasWidth/2 + xP
+      let yP_tl = canvasHeight/2 + yP
 
-      // Draw the sun and far Earth orbit
       ctx1.fillStyle = "orange";
       ctx1.font = "14px Verdana";
       ctx1.textAlign="center"; 
-      ctx1.fillText("The Sun", xSun, ySun + sunImg.height/2)
-      drawFarEarthOrbit();
-      drawSun();
+      ctx1.fillText("The Sun", xSun, ySun + sunImg.current.height/2)
     
-      // Draw the angle sweep line
-      ctx1.beginPath();
-      ctx1.moveTo(xO_tl, yO_tl);
-      ctx1.lineTo(xH_tl, 0);
-      ctx1.moveTo(xO_tl, yO_tl);
-      ctx1.lineTo(xE_tl, yE_tl);
-      ctx1.setLineDash([7, 3])
-      ctx1.lineWidth=2.0;
-      ctx1.strokeStyle = 'grey';
-      ctx1.stroke();
-
-      // Draw the Earth
-      ctx1.beginPath();
-      ctx1.arc(xE_tl, yE_tl, 5, 0, 2*Math.PI);
-      let grad = ctx1.createRadialGradient(xE_tl-1, yE_tl-2, 1,
-                                            xE_tl, yE_tl, 5);
-      grad.addColorStop(0, "#b3bdf8");
-      grad.addColorStop(1, "blue");
-      ctx1.fillStyle = grad;
-      ctx1.fill();
-
-      // Draw the foreground object
-      xO_tl = xSun + xO
-      yO_tl = yO
-      ctx1.beginPath();
-      ctx1.arc(xO_tl, yO_tl, 5, 0, 2*Math.PI);
-      grad = ctx1.createRadialGradient(xO_tl-1, yO_tl-2, 1,
-                                           xO_tl, yO_tl, 5);
-      grad.addColorStop(0, "white");
-      grad.addColorStop(1, "green");
-      ctx1.fillStyle = grad;
-      ctx1.fill();
-
-      // Draw the angle annotation
-      const ang = Math.atan(R/d_pix)
-      const parAngle_asec = px/8401;
-      const xArcEnd = xO_tl + annOff * Math.sin(ang)
-      const yArcEnd = yO_tl- annOff * Math.cos(ang)
-      ctx1.beginPath();
-      ctx1.setLineDash([5, 5])
-      ctx1.strokeStyle = "yellow";
-      ctx1.lineWidth=1.0;
-      ctx1.moveTo(xO_tl, yO_tl);
-      ctx1.lineTo(xArcEnd, yArcEnd);
-      ctx1.moveTo(xO_tl, yO_tl);
-      ctx1.lineTo(xO_tl, yO_tl-annOff);
-      ctx1.stroke();
-      ctx1.beginPath();
-      ctx1.setLineDash([1, 0])
-      ctx1.lineWidth=3.0;
-      ctx1.arc(xO_tl, yO_tl, annOff, -Math.PI/2, +ang-Math.PI/2);
-      ctx1.stroke();
-      //ctx1.stroke();
-      //ctx1.lineTo(xO_tl, yO_tl);
-      ctx1.fillStyle = "yellow";
-      ctx1.font = "14px Verdana";
-      ctx1.textAlign="start"; 
-      var annTxt = "Angle = " + parAngle_asec.toFixed(2) + "''"
-      ctx1.fillText(annTxt, xO_tl+annOff*Math.sin(ang)+ annOff*0.1,
-                    yO_tl-annOff*Math.cos(ang)/2);
-
-      // Draw projected orbit
-      ctx2.beginPath();
-      ctx2.setLineDash([7, 3])
-      ctx2.strokeStyle = 'grey';
-      ctx2.lineWidth=1.5;
-      ctx2.ellipse(viewCanvas.width/2, viewCanvas.height/2, R1,
-                   R1 * Math.cos(inc_deg * Math.PI / 180), 0, 0, 2*Math.PI)
-      ctx2.stroke();
-
-      // Draw projected angle line
-      ctx2.beginPath();
-      ctx2.setLineDash([3, 3])
-      ctx2.strokeStyle = 'yellow';
-      ctx2.lineWidth=1.0;
-      ctx2.moveTo(viewCanvas.width/2, viewCanvas.height/2);
-      ctx2.lineTo(viewCanvas.width/2+R1, viewCanvas.height/2);
-      ctx2.stroke();
-    
-      // Draw view of object
-      ctx2.beginPath();
-      ctx2.arc(xP_tl, yP_tl, 5, 0, 2*Math.PI);
-      grad = ctx1.createRadialGradient(xP_tl-1, yP_tl-2, 1,
-                                           xP_tl, yP_tl, 5);
-      grad.addColorStop(0, "white");
-      grad.addColorStop(1, "green");
-      ctx2.fillStyle = grad;
-      ctx2.fill();
-
-      // Print the month
-      var months = ["January", "February", "March", "April", "May", "June",
-                  "July", "August", "September", "October", "November",
-                  "December" ]
-      ctx2.beginPath();
-      ctx2.fillStyle = "grey";
-      ctx2.font = "14px Verdana";
-      ctx2.textAlign="start";
-      ctx2.fillText(months[Math.floor(theta_deg/30)], 8,
-                    viewCanvas.height-8)
-    
-      // Final annotations
-      ctx1.beginPath();
-      ctx1.fillStyle = "white";
-      ctx1.font = "14px Verdana";
-      ctx1.textAlign="start"; 
-      ctx1.fillText("Not to scale.", 8, mainCanvas.height - 8)
-      ctx1.fillText("Background stars.", 8, 12 + 8)
-      ctx2.beginPath();
-      ctx2.fillStyle = "white";
-      ctx2.font = "14px Verdana";
-      ctx2.textAlign="center"; 
-      ctx2.fillText("View from Earth through a telescope.",
-                    viewCanvas.width/2, 12 + 8)
+      drawBackground(ctx1);
+      drawBackground(ctx2);
+      drawFarEarthOrbit(ctx1);
+      drawSun(ctx1);
+      drawObject(ctx1, xSun, yO);
+      drawEarth(ctx1, xSun + xE.current, ySun + yE.current);
+      drawAngleSweepLine(ctx1, xO_tl, yO_tl, xE_tl, yE_tl);
+      drawForegroundObject(ctx1, xO_tl, yO_tl);
+      drawAngleAnnotation(ctx1, xO_tl, yO_tl);
+      drawProjectedOrbit(ctx2, R1);
+      drawProjectedAngleLine(ctx2, R1);
+      drawViewOfObject(ctx2, xP_tl, yP_tl);
+      printMonths(ctx2);
+      annotate(ctx1, "Not to scale.", 8, canvasWidth - 8)
+      annotate(ctx1, "Background stars.", 8, 12 + 8)
+      annotate(ctx2, "View from Earth through a telescope.",
+                canvasHeight/2, 12 + 8)  
+     
     };
-
-    function drawSun() {
-        ctx1.drawImage(sunImg, xSun-sunImg.width/4, ySun-sunImg.height/4,
-                      sunImg.width/2, sunImg.height/2);
-      }
-
-    function drawNearEarthOrbit() {
-        ctx1.ellipse(xSun, ySun, R, R * Math.sin(inc_deg * Math.PI / 180.0),
-                     0, 0, Math.PI)
-        ctx1.setLineDash([7, 3])
-        ctx1.lineWidth=2;
-        ctx1.strokeStyle = 'grey';
-        ctx1.stroke();
-      }
-             
-      function drawFarEarthOrbit() {
-        ctx1.ellipse(xSun, ySun, R, R * Math.sin(inc_deg * Math.PI / 180),
-                     0, Math.PI, 0)
-        ctx1.setLineDash([7, 3])
-        ctx1.lineWidth=1.5;
-        ctx1.strokeStyle = 'grey';
-        ctx1.stroke();
-  
-        // Annotate the Earth
-        ctx1.font = "14px Verdana";
-        ctx1.fillStyle = '#6b72f8';
-        ctx1.textAlign="end"; 
-        ctx1.fillText("Earth", xSun-R-10, ySun+4)
-        ctx1.textAlign="start"; 
-      
-      }
 
     const animate = () => {
       if (!isMounted.current) return;
       animationFrameId.current = requestAnimationFrame(animate);
-      theta_deg += dTheta_deg;
-      theta_deg %= 360;
-      const theta_rad = theta_deg * Math.PI / 180;
-      xE = R * Math.sin(theta_rad);
-      yE = R * Math.cos(theta_rad) * Math.sin(inclination * Math.PI / 180);
+
+      calculatePositions();
 
       drawAll();
 
       // Draw the Sun and orbit line in the foreground during Earth eclipse
-      if (theta_deg > 256 || theta_deg < 90) {
-        drawNearEarthOrbit();
+      if (theta_deg.current > 256 || theta_deg.current < 90) {
+        drawNearEarthOrbit(ctx1);
       } else {
-        drawSun();
-        drawNearEarthOrbit();
+        drawSun(ctx1);
+        drawNearEarthOrbit(ctx1);
       }
     };
 
-    let imagesLoaded = 0;
-    const totalImages = 2;
-
-    const startAnimationIfReady = () => {
-        imagesLoaded++;
-        if (imagesLoaded === totalImages) {
-            R = RtoPix(distance_pc);
-            animate();
-        }
-    };
-
-    sunImg.onload = startAnimationIfReady;
-    backgroundImg.onload = startAnimationIfReady;
+    if (sunImgLoaded && bgImgLoaded) {
+      animate();
+    }
   
     // Cleanup function
     return () => {
@@ -296,7 +330,7 @@ export const ParallaxSimulation = ({distance_pc, px}) => {
         cancelAnimationFrame(animationFrameId.current); // ✅ Properly cancels animation
       }
     };
-  }, [distance_pc, px]);
+  }, [sunImgLoaded, bgImgLoaded, distance_pc]);
 
   return (
     <div>
