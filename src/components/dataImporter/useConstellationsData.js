47557450349202213bdf07e43156781efdebb583
Aga <aga@cottagelabs.com>
2025-03-11 00:25:29 +0000

    remove the variable

--- src/components/dataImporter/useConstellationsData.js
+++ src/components/dataImporter/useConstellationsData.js
@@ -5,7 +5,7 @@ export const useConstellationsData = () => {
     const [data, setData] = useState(null);
     
     useEffect(() => {
-        json(`${process.env.PUBLIC_URL}/assets/data/constellations.json`).then(setData);
+        json(`/assets/data/constellations.json`).then(setData);
       }, []);
       
       return data;
